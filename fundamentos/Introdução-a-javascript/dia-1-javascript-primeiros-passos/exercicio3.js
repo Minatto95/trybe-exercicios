447533a1e88e2cfacf601f76d71329ea7f25fd5d
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-21 20:28:39 -0300

    adicioando exercicio 4

--- fundamentos/Introdução-a-javascript/dia-1-javascript-primeiros-passos/exercicio3.js
+++ fundamentos/Introdução-a-javascript/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -3,9 +3,9 @@ const valor2 = 20;
 const valor3 = 30;
 
 if (valor1 > valor2 && valor1 > valor3){
-    console.log((valor1) + " é maior que " + (valor2) + " e " + (valor3))
+    console.log((valor1) + " é maior que " + (valor2) + " e " + (valor3));
 } else if (valor2 > valor1 && valor2 > valor3){
-    console.log((valor2) + " é maior que " + (valor3) + " e " + (valor1))
+    console.log((valor2) + " é maior que " + (valor3) + " e " + (valor1));
 } else {
-    console.log((valor3) + " é maior que " + (valor2) + " e " + (valor1))
+    console.log((valor3) + " é maior que " + (valor2) + " e " + (valor1));
 }
