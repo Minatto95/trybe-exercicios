5db561f28ede74807ddd7d382c042162f81c6cb4
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-21 20:05:33 -0300

    retorno da função com 3 valores

--- fundamentos/Introdução-a-javascript/dia-1-javascript-primeiros-passos/exercicio2.js
+++ fundamentos/Introdução-a-javascript/dia-1-javascript-primeiros-passos/exercicio2.js
@@ -2,7 +2,7 @@ const valor1 = 5;
 const valor2 = 10;
 
 if (valor1 > valor2){
-    console.log((valor1) +" é maior.")
+    console.log((valor1) +" é maior que " + (valor2))
 } else if (valor2 > valor1){
-    console.log((valor2) +" é maior.")
+    console.log((valor2) +" é maior que " + (valor1))
 }
---
+++ fundamentos/Introdução-a-javascript/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+const valor1 = 10;
+const valor2 = 20;
+const valor3 = 30;
+
+if (valor1 > valor2 && valor1 > valor3){
+    console.log((valor1) + " é maior que " + (valor2) + " e " + (valor3))
+} else if (valor2 > valor1 && valor2 > valor3){
+    console.log((valor2) + " é maior que " + (valor3) + " e " + (valor1))
+} else {
+    console.log((valor3) + " é maior que " + (valor2) + " e " + (valor1))
+}
