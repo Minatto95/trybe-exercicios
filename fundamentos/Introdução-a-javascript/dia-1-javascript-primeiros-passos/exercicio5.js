35bafdc8397c7d7ee7d16874c09c64790f863c1b
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-21 20:47:34 -0300

    exercicio dos angulos

---
+++ fundamentos/Introdução-a-javascript/dia-1-javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,11 @@
+const angulo1 = 70;
+const angulo2 = 45;
+const angulo3 = 65;
+
+const somaDosAngulos = angulo1 + angulo2 + angulo3;
+
+if (somaDosAngulos === 180){
+    console.log(true);
+} else {
+    console.log(false);
+}
