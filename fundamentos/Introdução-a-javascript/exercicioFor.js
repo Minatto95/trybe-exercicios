c3218d0b1d93192e9888c4a6169c05b34e1a4de6
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-22 15:22:03 -0300

    for/of

--- fundamentos/Introdução-a-javascript/exercicioFor.js
+++ fundamentos/Introdução-a-javascript/exercicioFor.js
@@ -1,6 +1,13 @@
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for(let index = 0; index < groceryList.length; index += 1){
+// for(let index = 0; index < groceryList.length; index += 1){
 
-    console.log(groceryList[index])
+//     console.log(groceryList[index])
+// }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let nome of names){
+
+    console.log(nome)
 }
