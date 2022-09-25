b82789f70ed054e25dfb685bc8f7fe1bdeb95fed
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-25 18:00:02 -0300

    exercicio bubblesort

--- fundamentos/Introdução-a-javascript/dia-2-javascript-array-e-loopfor/exercicio-7.js
+++ fundamentos/Introdução-a-javascript/dia-2-javascript-array-e-loopfor/exercicio-7.js
@@ -1,27 +1,18 @@
-// Ordene o array numbers em ordem decrescente e imprima seus valores;
-
-
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newArray = [];
 
-
-for (let index = 1; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
   
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+  if (index + 1 < numbers.length) {
     
-    if (numbers[index] > numbers[secondIndex]) {
-      
-      let position = numbers[index];
-      
-      numbers[index] = numbers[secondIndex];
-      
-      numbers[secondIndex] = position;
-    }
+    newArray.push(numbers[index] * numbers[index + 1]);
+  
+  } else {
+    
+    newArray.push(numbers[index] * 2);
   }
 }
 
-console.log(numbers);
+console.log(newArray);
 
 
-
-  
