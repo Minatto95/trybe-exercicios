67a9c64f6dcff625337d5e20061ff1020e5fbef1
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-26 15:52:27 -0300

    objetos

---
+++ fundamentos/Introdução-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio.js
@@ -0,0 +1,21 @@
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    }
+
+}
+
+player.fullName = player.name +' '+ player.lastName
+
+
+console.log('A jogadora ' + player.fullName)
+
