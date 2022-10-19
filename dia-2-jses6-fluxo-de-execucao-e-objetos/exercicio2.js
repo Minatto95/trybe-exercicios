4cc66c8fd37cef959a4c07a1b11d13d38d0b6302
Allan Medeiros Minato <allan.minato@gmail.com>
2022-10-19 00:21:36 -0300

    exercicio focado para o projeto

---
+++ dia-2-jses6-fluxo-de-execucao-e-objetos/exercicio2.js
@@ -0,0 +1,19 @@
+const coolestTvShow = {
+    name: "BoJack Horseman",
+    genre: "adult animation",
+    createdBy: "Raphael Bob-Waksberg",
+    favoriteCharacter: "Princess Carolyn",
+    quote: "Princess Carolyn always lands on her feet.",
+    seasons: 6,
+  };
+  
+//   const list = (tvShow)=>{
+//     let listOf = Object.values(tvShow);
+//   for (const property in listOf) {
+//     console.log(listOf[property]);
+//   }
+// }
+
+// list(coolestTvShow)
+
+console.log(Object.values(coolestTvShow));
